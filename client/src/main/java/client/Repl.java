f06fc30782a583f29346120c2e9b5664939b0e8a
unknown <rex.okland@gmail.com>
2024-07-29 10:54:18 -0600

    added more files for phase 5

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,13 @@
+package client;
+
+public class Repl {
+    private ChessClient client;
+
+    public Repl(String serverURL){
+        //todo: implement
+    }
+
+    public void run(){
+        //todo: implement
+    }
+}
